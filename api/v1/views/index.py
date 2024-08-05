a29bbb10163946f9084c823c49dfc0e28515cdf0
NecheHephzibah <neche.bless@gmail.com>
2024-08-05 11:21:07 -0700

    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status')
 def api_status():
     """
@@ -14,6 +15,7 @@ def api_status():
 
     resp = {'status': "OK"}
     return jsonify(resp)
+
 
 @app_views.route('/stats')
 def retrieve_stats():
