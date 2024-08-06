f542950631fb00b5f4291d666dfc33ca7c2ce196
NecheHephzibah <neche.bless@gmail.com>
2024-08-06 11:47:16 -0700

    Pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,7 @@
 Create Flask app blueprint, instance of Flask.
 """
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import specific route modules, ensuring no circular imports
 from api.v1.views import index
 from api.v1.views import states
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
